84658927404047f8d6fce70b32118c1c09b14482
Steve Haran <sharan@homeaway.co.uk>
2015-06-01 17:42:33 +0100

    wow

--- app/app.js
+++ app/app.js
@@ -1,6 +1,25 @@
 (function () {
     "use strict";
-    var app = angular.module("productManagement",
-        ["common.services",
-        "productResourceMock"]);
+    var app = angular.module(
+        "productManagement",
+        [
+            "common.services",
+            "ui.router",
+            "productResourceMock"
+        ]);
+
+    app.config([
+            "$stateProvider",
+            "$urlRouterProvider",
+        function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/products");
+
+            $stateProvider
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+        }]
+    );
 }());
