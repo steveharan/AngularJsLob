bdd1e5f3512a68fe8b250526f7f627315888753e
Steve Haran <sharan@homeaway.co.uk>
2015-06-02 17:39:22 +0100

    Firebase integration started

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@
             "ui.router",
             "ui.mask",
             "ui.bootstrap",
+            "firebase",
             "productResourceMock"
         ]);
 
--- app/products/Edit/productEditCtrl.js
+++ app/products/Edit/productEditCtrl.js
@@ -4,12 +4,19 @@
         angular
             .module("productManagement")
             .controller("ProductEditCtrl",
-                        ["product", "$state",ProductEditCtrl]);
+                        ["product", "$state", "$firebaseObject", ProductEditCtrl]);
 
-        function ProductEditCtrl(product, $state) {
+        function ProductEditCtrl(product, $state, $firebaseObject) {
+            var ref = new Firebase("https://steveharan.firebaseio.com/")
+
             var vm = this;
 
             vm.product = product;
+
+            var productsRef = ref.child("products");
+            productsRef.set(
+                vm.product.toJSON()
+            );
 
             if (vm.product && vm.product.productId) {
                 vm.title = "Edit: " + vm.product.productName;
