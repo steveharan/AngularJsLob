8ac2d28a953a3812477c467358c7ebe0028635a7
Steve Haran <sharan@homeaway.co.uk>
2015-06-04 15:06:35 +0100

    checkpoint

--- app/products/Edit/productEditCtrl.js
+++ app/products/Edit/productEditCtrl.js
@@ -7,6 +7,24 @@
                         ["product", "$state", "$firebaseObject", ProductEditCtrl]);
 
         function ProductEditCtrl(product, $state, $firebaseObject) {
+            var toasterOptions = {
+                "closeButton": false,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-bottom-full-width",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+
             var ref = new Firebase("https://steveharan.firebaseio.com/")
 
             var vm = this;
@@ -27,25 +45,37 @@
                 vm.opened = !vm.opened;
             }
 
-            vm.submit = function () {
-                var productsRef = ref.child("products");
-                if (vm.product && vm.product.productId) {
-                    productsRef.set(
-                        vm.product.toJSON()
-                    );
+            vm.submit = function (isValid ) {
+                if (isValid) {
+                    vm.product.$save(function (data) {
+                        toastr.options = toasterOptions;
+                        toastr.success("Save Successful");
+                    });
                 }
                 else {
-                    productsRef.push(
-                        vm.product.toJSON()
-                    );
+                    toastr.options = toasterOptions;
+                    toastr.error("Form is invalid");
                 }
-                vm.product.$save();
             }
 
             vm.cancel = function () {
                 $state.go('productList');
             }
 
+            vm.addTags = function (tags) {
+                if (tags) {
+                    var array = tags.split(',');
+                    vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                    vm.newTags = "";
+                }
+                else {
+                    alert("Please enter some tags");
+                }
+            }
+
+            vm.removeTag = function (idx) {
+                vm.product.tags.splice(idx, 1);
+            }
 
         }
     }()
